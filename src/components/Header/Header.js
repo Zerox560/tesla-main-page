f31742bb34240681dc679d3e2fff4d5cdbded953
Ramiro <84472574+Zerox560@users.noreply.github.com>
2022-01-06 03:37:18 -0300

    Proyecto Terminado

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,49 @@
-import { Container, Menu, RightMenu, CustomMenu } from "./HeaderStyles";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCars } from "../../features/car/carSlice";
+
+import { Container, Menu, RightMenu, CustomMenu, SideBarMenu, CloseBtnContainer, CloseButton } from "./HeaderStyles";
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const cars = useSelector(selectCars);
+
+    const menuHandler = () => { setMenuOpen(prevState => !prevState) }
+
     return (
         <Container>
             <a href="/">
                 <img src="/images/logo.svg" alt="Logo de tesla" />
             </a>
             <Menu>
-                <a href="/">Model S</a>
-                <a href="/">Model 3</a>
-                <a href="/">Model S</a>
-                <a href="/">Model Y</a>
+                {cars && cars.map((car, index) => (
+                    <a href="/" key={index}>{car}</a>
+                ))}
             </Menu>
             <RightMenu>
                 <a href="/">Shop</a>
                 <a href="/">Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={menuHandler} />
             </RightMenu>
+            <SideBarMenu open={menuOpen} >
+                <CloseBtnContainer>
+                    <CloseButton onClick={menuHandler} />
+                </CloseBtnContainer>
+                <li><a href="/">Existing Inventory</a></li>
+                <li><a href="/">Used Inventory</a></li>
+                <li><a href="/">Trade-In</a></li>
+                <li><a href="/">Test Drive</a></li>
+                <li><a href="/">Cybertruck</a></li>
+                <li><a href="/">Roadster</a></li>
+                <li><a href="/">Semi</a></li>
+                <li><a href="/">Charging</a></li>
+                <li><a href="/">Powerwall</a></li>
+                <li><a href="/">Commercial Energy</a></li>
+                <li><a href="/">Utilities</a></li>
+                <li><a href="/">Find Us</a></li>
+                <li><a href="/">Support</a></li>
+                <li><a href="/">Investor Relations</a></li>
+            </SideBarMenu>
         </Container>
     )
 }
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 export const Container = styled.div`
+    z-index: 1;
     top: 0;
     left: 0;
     right: 0;
@@ -36,9 +38,46 @@ export const RightMenu = styled.div`
 
     a {
         font-weight: 600;
+        
+        @media (max-width: 768px) {
+            display: none;
+        }
     }
+
 `;
 
 export const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+`;
+
+export const SideBarMenu = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: #ffff;
+    width: 300px;
+    z-index: 100;
+    list-style: none;
+    padding: 20px;
+    text-align: start;
+    overflow-y: scroll;
+    transition: all .4s ease;
+    transform: ${({ open }) => open ? "translateX(0)" : "translateX(100%)"};
+
+    li {
+        padding: 15px 0;
+        font-weight: 600;
+        border-bottom: solid 1px rgba(0, 0, 0, .2);
+    }
+}
+`;
+
+export const CloseBtnContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+export const CloseButton = styled(CloseIcon)`
     cursor: pointer;
 `;
